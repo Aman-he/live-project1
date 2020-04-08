d501db70cff7b6e39f3f9c83ae3a826b80493e47
HanYuChen4613 <62946652+HanYuChen4613@users.noreply.github.com>
2020-04-08 21:32:44 +0800

    Update 保存.cpp

--- 保存.cpp
+++ 保存.cpp
@@ -2,13 +2,14 @@ void save(){
 	int i;
 	FILE *fp;
 	char filename[LEN + 1];
-	printf("请输入要保存的文件名：\n");
+	printf("璇疯緭鍏ヨ淇濆瓨鐨勬枃浠跺悕锛歕n");
 	scanf("%s", filename);
 	fp = fopen(filename, "w");
 	for (i = 0; i < n; i++){
 		fprintf(fp, "%s%s%s%s\n", stu[i].StudentId, stu[i].StudentName, stu[i].StudentEnter, stu[i].StudentLeave);
 	}
-	printf("保存成功！！！\n");
+	printf("淇濆瓨鎴愬姛锛侊紒锛乗n");
 	fclose(fp);
 	system("pause");
 }
+/
